62ec7a6119f6177fcaabd91500f967da4de2cc19
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-16 09:00:17 -0500

    Modifique el mensaje de confirmacion mostrado al intentar eliminar un estudiante.

--- src/Utilities/OutputMessages.java
+++ src/Utilities/OutputMessages.java
@@ -426,6 +426,13 @@ public class OutputMessages {
     public String EliminacionExitosa() { return "Se ha eliminado la organización exitosamente"; }
 
     /**
+     * Mensaje mostrado cuando se intente eliminar une estudiante de
+     * la base de datos
+     * @return una cadena con el mensaje de confirmacion
+     */
+    public String EliminarEstudianteConfirmation() { return "¿Estas seguro que deseas eliminar el estudiante?"; }
+
+    /**
      * mensaje mostrado cuando se introduce una selección nula
      * en la tabla de visualizar reportes.
      * @return una cadena con el mensaje de error
