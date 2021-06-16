dcef3cae2ea46adfa19bcc2a735c7619e66914b9
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-16 08:13:40 -0500

    Agregue comentarios a la clase.

--- src/Utilities/SelectionContainer.java
+++ src/Utilities/SelectionContainer.java
@@ -10,6 +10,9 @@ package Utilities;
 
 import Entities.*;
 
+/**
+ * Clase utilizada para pasar entidades de una pantalla a otra
+ */
 public final class SelectionContainer {
     private static SelectionContainer selectionContainer;
     private Proyecto proyectoElegido;
@@ -20,6 +23,9 @@ public final class SelectionContainer {
     private ArchivoConsulta[] archivosConsulta;
     private ConjuntoDocenteInforme conjuntoDocenteInforme;
 
+    /**
+     * Constructor privado default de la clase
+     */
     private SelectionContainer() {
         this.estudianteElegido = null;
         this.proyectoElegido = null;
@@ -30,6 +36,10 @@ public final class SelectionContainer {
         this.conjuntoDocenteInforme = null;
     }
 
+    /**
+     * Retorna una intancia de la clase
+     * @return
+     */
     public static SelectionContainer GetInstance() {
         if( selectionContainer == null ) {
             selectionContainer = new SelectionContainer();
@@ -37,68 +47,124 @@ public final class SelectionContainer {
         return selectionContainer;
     }
 
+    /**
+     * Regresa el proyecto almacenado en la clase
+     * @return un proyecto
+     */
     public Proyecto getProyectoElegido() {
         return proyectoElegido;
     }
 
+    /**
+     * Regresa el estudiante almacenado en la clase
+     * @return un estudiante
+     */
     public Estudiante getEstudianteElegido() {
         return estudianteElegido;
     }
 
+    /**
+     * Regresa el resposnable almacenado en la clase
+     * @return un responsableProyecto
+     */
     public ResponsableProyecto getResponsableElegido() {
         return responsableElegido;
     }
 
+    /**
+     * Regresa la organizacion vinculada almacenada en la clase
+     * @return una organizacion vinculada
+     */
     public OrganizacionVinculada getOrganizacionElegida(){
         return organizacionElegida;
     }
 
+    /**
+     * Regresa un arreglo de los archivos consulta almacenados en la clase
+     * @return un arreglo de archivos consulta
+     */
     public ArchivoConsulta[] getArchivosConsulta() {
         return archivosConsulta;
     }
 
+    /**
+     * Regresa el conjunto docente informe almacenado en la clase
+     * @return un ConjuntoDocenteInforme
+     */
     public ConjuntoDocenteInforme getConjuntoDocenteInforme(){return conjuntoDocenteInforme;}
 
+    /**
+     * Hace un set de conjunto docente informe que sera utilizado en otra pantalla
+     * @param conjuntoDocenteInforme el conjunto docente informe que sera utilizado en otra pantalla
+     */
     public void setConjuntoDocenteInforme(ConjuntoDocenteInforme conjuntoDocenteInforme){
         if( conjuntoDocenteInforme != null ){
             this.conjuntoDocenteInforme = conjuntoDocenteInforme;
         }
     }
 
+    /**
+     * Hace un set de proyecto que sera utilizado en otra pantalla
+     * @param proyectoElegido el proyecto que sera utilizado en otra pantalla
+     */
     public void setProyectoElegido(Proyecto proyectoElegido) {
         if( proyectoElegido != null ) {
             this.proyectoElegido = proyectoElegido;
         }
     }
 
+    /**
+     * Retorna el informe de problema almacenado en la clase
+     * @return el informe problema
+     */
     public InformeProblema getInformeElegido(){
         return informeElegido;
     }
 
+    /**
+     * Hace un set del estudiante que sera utilizada en otra pantalla
+     * @param estudianteElegido el estudiante que sera utilizada en otra pantalla
+     */
     public void setEstudianteElegido(Estudiante estudianteElegido) {
         if( estudianteElegido != null ) {
             this.estudianteElegido = estudianteElegido;
         }
     }
 
+    /**
+     * Hace un set de la organizacion que sera utilizada en otra pantalla
+     * @param organizacionElegida la organizacion que sera utilizada en otra pantalla
+     */
     public void setOrganizacionElegida( OrganizacionVinculada organizacionElegida ){
         if( organizacionElegida != null ) {
             this.organizacionElegida = organizacionElegida;
         }
     }
 
+    /**
+     * Hace un set de un responsable que sera utilizado en otra pantalla
+     * @param responsableElegido el responsable que sera utilizado en otra pantalla
+     */
     public void setResponsableElegido( ResponsableProyecto responsableElegido ){
         if( responsableElegido != null ) {
             this.responsableElegido = responsableElegido;
         }
     }
 
+    /**
+     * Hace un set de un informe que sera utilizado en otra pantalla
+     * @param informeElegido el informe que sera utilizado en otra pantalla
+     */
     public void setInformeElegido( InformeProblema informeElegido ){
         if( informeElegido != null ) {
             this.informeElegido = informeElegido;
         }
     }
 
+    /**
+     * Hace un set de los archivos de consula que seran utilizados en otras pantallas
+     * @param archivosConsulta los archivos consulta que seran utilizados en otras pantallas
+     */
     public void setArchivosConsulta(ArchivoConsulta[] archivosConsulta) {
         this.archivosConsulta = archivosConsulta;
     }
